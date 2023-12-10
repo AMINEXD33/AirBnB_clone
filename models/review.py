f0ebd69851e7ef05d6b8caae9b13157e43c06ed1
MohammedMaghraoui <mohamadmaghraoui99@gmail.com>
2023-12-10 13:45:55 -0800

    all file

--- models/review.py
+++ models/review.py
@@ -2,15 +2,18 @@
 """Defines the Review class."""
 from models.base_model import BaseModel
 
+
 class Review(BaseModel):
     """Represent a Review.
     """
+
     place_id = ""
     user_id = ""
-    text = ""    
+    text = ""
+
     def __init__(self, *args, **kwargs):
         super().__init__(**kwargs)
         self.place_id = Review.place_id
         self.user_id = Review.user_id
         self.text = Review.text
-        self.__SPinit__(**kwargs)    
+        self.__SPinit__(**kwargs)
